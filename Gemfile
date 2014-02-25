b780e507f63d2e9eff260dbde9bc478b1d687ad3
Abass Zakari <developer.meltwater@gmail.com>
2014-02-25 00:56:40 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.3'
+
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3'
+
+group :development do
+	gem 'rspec-rails', '2.0.0.beta.18'
+end
+
+group :test do
+	gem 'rspec', '2.0.0.beta.18'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '70d498d6ad480d932a91efdc3591ed63855a99965285d17d8e55824a7332097fbc66a1ce71b9b7e08665578ec6d8891035e6c28ceaeb4dbb8a4f65e01f8d07cd'
